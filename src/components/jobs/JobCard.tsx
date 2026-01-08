79663220fe31712b03f1387ef16601383baca61b
Rajeev Sharma <freelancer.rajeev61@gmail.com>
2026-01-08 06:27:26 +0000

    why job photo I am not getting here in job card check and fix it

--- src/components/jobs/JobCard.tsx
+++ src/components/jobs/JobCard.tsx
@@ -414,7 +414,7 @@ export function JobCard({ job, technicianId, onJobsUpdate }: { job: Job, technic
                 <Dialog>
                     <DialogTrigger asChild>
                         <div className="relative h-12 w-12 rounded-md overflow-hidden cursor-pointer flex-shrink-0">
-                            <Image src={job.media_url} alt="Job photo" fill className="object-cover"/>
+                            <Image src={job.media_url} alt="Job photo" layout="fill" className="object-cover"/>
                         </div>
                     </DialogTrigger>
                     <DialogContent className="p-0 border-0 max-w-screen-md">
@@ -459,5 +459,3 @@ export function JobCard({ job, technicianId, onJobsUpdate }: { job: Job, technic
     </>
   );
 }
-
-    
