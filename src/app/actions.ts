9d9f22b547de5187fdb1afe2e4946f7b4d9573b1
rajeev <rajeev06code>
2026-01-09 22:11:55 +0530

    done

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -53,7 +53,7 @@ export async function updateJobStatusAction(payload: UpdateStatusPayload) {
     }
     
     revalidatePath('/jobs');
-  console.log("revalidated /jobs path");
+  console.log("revalidated /jobs");
     const data = await response.json();
     return data;
 
